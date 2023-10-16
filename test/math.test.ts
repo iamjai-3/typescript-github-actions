e8665b04527b1fe6cc29f190a66e6edb0b703fa7
iamjai <jai.develop3r@gmail.com>
2023-10-16 18:12:21 +0530

    test 2

--- test/math.test.ts
+++ test/math.test.ts
@@ -7,7 +7,7 @@ describe('Math functions', () => {
   });
 
   it('should add 5 by 3', () => {
-    const result = add(5, 4);
+    const result = add(5, 5);
     expect(result).toEqual(8);
   });
 });
